b9c9f0a6daaae32b85d716cad1aeaf01aa73e6ef
ImperatorNeron <katebarspk@gmail.com>
2024-08-30 18:32:15 +0300

    # 15 | Added simple ping

---
+++ core/api/urls.py
@@ -0,0 +1,15 @@
+from django.http import HttpRequest
+from django.urls import path
+from ninja import NinjaAPI
+
+api = NinjaAPI()
+
+
+@api.get("/ping")
+def ping(request: HttpRequest):
+    return {"result": True}
+
+
+urlpatterns = [
+    path("v1/", api.urls),
+]
