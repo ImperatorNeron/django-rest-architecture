6b0a8ad02bda243b1e588710ea907ecffecd9747
ImperatorNeron <katebarspk@gmail.com>
2024-08-31 22:40:48 +0300

    # 20 | Added to_entity method to models

--- core/apps/companies/models/company_types.py
+++ core/apps/companies/models/company_types.py
@@ -1,11 +1,16 @@
 from django.db import models
 
+from core.apps.companies.entities.company_types import CompanyTypeEntity
+
 
 class CompanyType(models.Model):
     title = models.CharField(
         verbose_name="Назва компанії",
         max_length=255,
     )
+
+    def to_entity(self) -> CompanyTypeEntity:
+        return CompanyTypeEntity(id=self.pk, title=str(self.title))
 
     def __str__(self):
         return f"{self.title}"
