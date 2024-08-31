37845f9650a3333faef1a6dfcc7e0ccbfddaf177
ImperatorNeron <katebarspk@gmail.com>
2024-09-01 00:43:33 +0300

    # 25 | Added pagination to services and handler

--- core/api/v1/companies/handlers.py
+++ core/api/v1/companies/handlers.py
@@ -1,10 +1,10 @@
 from django.http import HttpRequest
-from ninja import Router
+from ninja import Router, Query
 
-from core.api.v1.companies.schemas.company_type_schema import (
-    CompanyTypeListSchema,
-    CompanyTypeSchema,
-)
+from core.api.pagination import PaginationOut, PaginationIn
+from core.api.schemas import ListPaginatedResponse, ApiResponse
+from core.api.v1.companies.schemas.company_type_schema import CompanyTypeSchema
+
 from core.apps.companies.services.company_types import (
     BaseCompanyTypeService,
     ORMCompanyTypeService,
@@ -13,8 +13,22 @@ from core.apps.companies.services.company_types import (
 router = Router(tags=["Companies"])
 
 
-@router.get("/types", response=CompanyTypeListSchema)
-def get_company_type_list_handler(request: HttpRequest) -> CompanyTypeListSchema:
+@router.get("/types", response=ApiResponse[ListPaginatedResponse[CompanyTypeSchema]])
+def get_company_type_list_handler(
+    request: HttpRequest,
+    pagination_in: Query[PaginationIn],
+) -> ApiResponse[ListPaginatedResponse[CompanyTypeSchema]]:
     service: BaseCompanyTypeService = ORMCompanyTypeService()
-    company_types_list = service.get_list_company_types()
-    return [CompanyTypeSchema.from_entity(obj) for obj in company_types_list]
+    types_list = service.get_list_company_types(pagination_in)
+    items = [CompanyTypeSchema.from_entity(obj) for obj in types_list]
+    pagination_out = PaginationOut(
+        offset=pagination_in.offset,
+        limit=pagination_in.limit,
+        total=service.get_company_types_count(),
+    )
+    return ApiResponse(
+        data=ListPaginatedResponse(
+            items=items,
+            pagination=pagination_out,
+        )
+    )
--- core/apps/companies/services/company_types.py
+++ core/apps/companies/services/company_types.py
@@ -1,16 +1,29 @@
 from abc import ABC, abstractmethod
 from typing import Iterable
+
+from core.api.pagination import PaginationIn
 from core.apps.companies.entities.company_types import CompanyTypeEntity
 from core.apps.companies.models.company_types import CompanyType as CompanyTypeModel
 
 
 class BaseCompanyTypeService(ABC):
     @abstractmethod
-    def get_list_company_types(self) -> Iterable[CompanyTypeEntity]:
+    def get_list_company_types(self, pagination: PaginationIn) -> Iterable[CompanyTypeEntity]:
+        ...
+
+    @abstractmethod
+    def get_company_types_count(self) -> int:
         ...
 
 
 class ORMCompanyTypeService(BaseCompanyTypeService):
-    def get_list_company_types(self) -> Iterable[CompanyTypeEntity]:
-        qs = CompanyTypeModel.objects.filter()
+    def get_list_company_types(
+        self, pagination: PaginationIn
+    ) -> Iterable[CompanyTypeEntity]:
+        qs = CompanyTypeModel.objects.filter()[
+            pagination.offset: pagination.offset + pagination.limit
+        ]
         return [company_type.to_entity() for company_type in qs]
+
+    def get_company_types_count(self) -> int:
+        return CompanyTypeModel.objects.filter().count()
