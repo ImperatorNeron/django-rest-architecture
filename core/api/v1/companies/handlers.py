a8ff1edeff52051d03224ba985392820c1261227
ImperatorNeron <katebarspk@gmail.com>
2024-08-31 22:41:55 +0300

    # 21 | Added handler to getting company type list

--- core/api/v1/companies/handlers.py
+++ core/api/v1/companies/handlers.py
@@ -1,11 +1,20 @@
 from django.http import HttpRequest
 from ninja import Router
 
-from core.api.v1.companies.schemas import CompanyTypeListSchema
+from core.api.v1.companies.schemas.company_type_schema import (
+    CompanyTypeListSchema,
+    CompanyTypeSchema,
+)
+from core.apps.companies.services.company_types import (
+    BaseCompanyTypeService,
+    ORMCompanyTypeService,
+)
 
 router = Router(tags=["Companies"])
 
 
 @router.get("/types", response=CompanyTypeListSchema)
-def get_product_list_handler(request: HttpRequest) -> CompanyTypeListSchema:
-    return []
+def get_company_type_list_handler(request: HttpRequest) -> CompanyTypeListSchema:
+    service: BaseCompanyTypeService = ORMCompanyTypeService()
+    company_types_list = service.get_list_company_types()
+    return [CompanyTypeSchema.from_entity(obj) for obj in company_types_list]
